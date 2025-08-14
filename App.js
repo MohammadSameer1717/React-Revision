02e88f8f2fbff0f3ee802c89c23aff00bcd46518
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-08-15 01:44:02 +0530

    React-Revision 01

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I'm an h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I'm an h2 tag"),
+  ]),
+]);
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+console.log(parent); // object
 
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
