994842b79f2613c8fc39a0bb5379027e0080e9bb
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-09-04 22:34:20 +0530

    Lecture-06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,30 +1,53 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
  
 const Body = () => {
   // local store variable = superpowerFull variable
-  const [listOfReataurants, setListOfRestaurant] =useState (resList);
+  const [listOfReataurants, setListOfRestaurant] =useState ([]);
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendered");
   
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "http://swiggy"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurant(json?.cards[2]?.data?.data?.cards);
+    
+  };
+  
+  return listOfReataurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button 
-        className="filter-btn"
-        onClick={() => {
-          listOfReataurants = listOfReataurants.filter(
-            (res) => res.data.aveRating > 4
-          );
-          console.log(listOfReataurants);
-          }}
-        >
-          Top Reted Restaurants
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+
+        <button className="filter-btn" onClick={handleTopRated}>
+          Top Rated Restaurants  
         </button>
       </div>
+
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
+import{ LOGO_URL } from "../utils/constants";
+
 // Header Component
 const Header = () => {
+  let btnName = "Login";
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fast-food-logo%2Crestaurant-logo%2Crestaurant-ico-design-template-b7778b37cd176f87af34666bad99eb58_screen.jpg?ts=1672672947"
-          alt="logo"
-        />
+        <img className="logo" scr= {LOGO_URL} />
       </div>
       <div className="nav-item">
         <ul>
@@ -15,6 +15,14 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+          className="Login"
+          onClick={() => {
+            btnName = "Logout";
+          }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
